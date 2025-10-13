a210aab5d877b69a3494139886f086fcf8039fe7
Yury Mediro <sagn375@gmail.com>
2025-10-13 16:31:31 +0300

    feat: create zod song schema

---
+++ client-side/src/features/ValidateSchema/SongCreateSchema.ts
@@ -0,0 +1,22 @@
+import * as z from 'zod'
+
+export const SongCreateSchema = z.object({
+	title: z
+		.string()
+		.min(1, { message: 'Title is required' })
+		.max(20, { message: 'Max 20 characters' })
+		.refine(value => value.trim().length > 0, {
+			message: 'Title cannot consist only of spaces',
+		}),
+	artist: z
+		.string()
+		.min(1, { message: 'Artist is required' })
+		.max(20, { message: 'Max 20 characters' })
+		.refine(value => value.trim().length > 0, {
+			message: 'Artist cannot consist only of spaces',
+		}),
+	imageFile: z.instanceof(File).nullable(),
+	audioFile: z.instanceof(File).nullable(),
+	albumId: z.string().optional(),
+	duration: z.string().default('0'),
+})
