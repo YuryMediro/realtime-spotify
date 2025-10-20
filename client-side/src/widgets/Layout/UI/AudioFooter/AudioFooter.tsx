ab688a57b704e2f6851d1d9297e3c9424db21e87
Yury Mediro <sagn375@gmail.com>
2025-10-20 15:54:31 +0300

    feat: add mobileSheetFooter

--- client-side/src/widgets/Layout/UI/AudioFooter/AudioFooter.tsx
+++ client-side/src/widgets/Layout/UI/AudioFooter/AudioFooter.tsx
@@ -18,6 +18,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/kit/popover";
+import { MobileSheetFooter } from "./MobileSheetFooter";
 
 export const AudioFooter = observer(() => {
   const {
@@ -38,139 +39,143 @@ export const AudioFooter = observer(() => {
   const [isHoverTime, setIsHoverTime] = useState(false);
 
   return (
-    <footer className="h-20 bg-zinc-900 px-4 relative mx-4 rounded-lg">
-      <div className="absolute -top-13 left-0 right-0 flex justify-between text-md text-zinc-400 px-2">
-        {isHoverTime && (
-          <>
-            <span className="text-white border border-zinc-600 rounded-md px-3 py-2 bg-zinc-900">
-              {formatDuration(currentTime)}
-            </span>
-            <span className="text-white border border-zinc-600 rounded-md px-3 py-2 bg-zinc-900">
-              {formatDuration(duration)}
-            </span>
-          </>
-        )}
-      </div>
-
-      <div
-        className="absolute left-0 right-0 h-1"
-        onMouseEnter={() => setIsHoverTime(true)}
-        onMouseLeave={() => setIsHoverTime(false)}
-      >
-        <Slider
-          value={[currentTime]}
-          max={duration || 100}
-          step={1}
-          onValueChange={(value) => seek(value[0])}
-          className="w-full h-full hover:cursor-grab active:cursor-grabbing"
-          trackClassName="h-1 bg-zinc-600"
-          rangeClassName="bg-green-500"
-          thumbClassName="w-3 h-3 bg-green-500 border-none"
-        />
-      </div>
-
-      <div className="flex justify-between items-center h-full mx-auto">
-        <div className="flex items-center gap-4">
-          {currentSong && (
+    <>
+      <footer className="hidden sm:block h-20 bg-zinc-900 px-4 relative mx-4 rounded-lg">
+        <div className="absolute -top-13 left-0 right-0 flex justify-between text-md text-zinc-400 px-2">
+          {isHoverTime && (
             <>
-              <img
-                src={currentSong.imageUrl}
-                alt={currentSong.title}
-                className="w-14 h-14 object-cover rounded-md"
-              />
-              <div className="flex-1 min-w-0">
-                <div className="font-medium truncate hover:underline cursor-pointer">
-                  {currentSong.title}
-                </div>
-                <div className="text-sm text-zinc-400 truncate hover:underline cursor-pointer">
-                  {currentSong.artist}
-                </div>
-              </div>
+              <span className="text-white border border-zinc-600 rounded-md px-3 py-2 bg-zinc-900">
+                {formatDuration(currentTime)}
+              </span>
+              <span className="text-white border border-zinc-600 rounded-md px-3 py-2 bg-zinc-900">
+                {formatDuration(duration)}
+              </span>
             </>
           )}
         </div>
 
-        <div className="flex items-center gap-4">
-          <Button
-            size="icon"
-            variant="ghost"
-            className="hidden sm:inline-flex hover:text-white text-zinc-400"
-          >
-            <Shuffle className="h-4 w-4" />
-          </Button>
-          <div className="flex items-center gap-4 sm:gap-2">
-            <Button
-              size="icon"
-              variant="ghost"
-              className="hover:text-white text-zinc-400 hidden sm:flex"
-              onClick={playPrev}
-              disabled={!canGoPrev}
-            >
-              <SkipBack className="h-4 w-4" />
-            </Button>
-
-            <Button
-              size="icon"
-              className="bg-green-500 hover:bg-green-600 text-black rounded-full h-10 w-10"
-              onClick={togglePlay}
-              disabled={!currentSong}
-            >
-              {isPlaying ? (
-                <Pause className="h-5 w-5" />
-              ) : (
-                <Play className="h-5 w-5" />
-              )}
-            </Button>
-
-            <Button
-              size="icon"
-              variant="ghost"
-              className="hover:text-white text-zinc-400 hidden sm:flex"
-              onClick={playNext}
-              disabled={!canGoNext}
-            >
-              <SkipForward className="h-4 w-4" />
-            </Button>
-          </div>
-          <Button
-            size="icon"
-            variant="ghost"
-            className="hidden sm:inline-flex hover:text-white text-zinc-400"
-          >
-            <Repeat className="h-4 w-4" />
-          </Button>
+        <div
+          className="absolute left-0 right-0 h-1"
+          onMouseEnter={() => setIsHoverTime(true)}
+          onMouseLeave={() => setIsHoverTime(false)}
+        >
+          <Slider
+            value={[currentTime]}
+            max={duration || 100}
+            step={1}
+            onValueChange={(value) => seek(value[0])}
+            className="w-full h-full hover:cursor-grab active:cursor-grabbing"
+            trackClassName="h-1 bg-zinc-600"
+            rangeClassName="bg-green-500"
+            thumbClassName="w-3 h-3 bg-green-500 border-none"
+          />
         </div>
 
-        <div className="hidden sm:flex items-center gap-2">
-          <Popover>
-            <PopoverTrigger asChild>
+        <div className="flex justify-between items-center h-full mx-auto">
+          <div className="flex items-center gap-4">
+            {currentSong && (
+              <>
+                <img
+                  src={currentSong.imageUrl}
+                  alt={currentSong.title}
+                  className="w-14 h-14 object-cover rounded-md"
+                />
+                <div className="flex-1 min-w-0">
+                  <div className="font-medium truncate hover:underline cursor-pointer">
+                    {currentSong.title}
+                  </div>
+                  <div className="text-sm text-zinc-400 truncate hover:underline cursor-pointer">
+                    {currentSong.artist}
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+
+          <div className="flex items-center gap-4">
+            <Button
+              size="icon"
+              variant="ghost"
+              className="inline-flex hover:text-white text-zinc-400"
+            >
+              <Shuffle className="h-4 w-4" />
+            </Button>
+            <div className="flex items-center gap-2">
               <Button
                 size="icon"
                 variant="ghost"
-                className="hover:text-white text-zinc-400"
+                className="hover:text-white text-zinc-400 flex"
+                onClick={playPrev}
+                disabled={!canGoPrev}
               >
-                <Volume1 className="h-7 w-7" />
+                <SkipBack className="h-4 w-4" />
               </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-3 bg-zinc-800 border-zinc-700">
-              <Slider
-                className="h-32 hover:cursor-grab active:cursor-grabbing text-green-500"
-                orientation="vertical"
-                step={1}
-                max={100}
-                value={[volume]}
-                onValueChange={(value) => setVolume(value[0])}
-                trackClassName="w-1 bg-zinc-600"
-                rangeClassName=" bg-green-500"
-                thumbClassName="w-3 h-3 bg-white bg-green-500 border-none"
-              />
-              <div className="text-xs text-white text-center mt-2">
-                {volume}%
-              </div>
-            </PopoverContent>
-          </Popover>
+
+              <Button
+                size="icon"
+                className="bg-green-500 hover:bg-green-600 text-black rounded-full h-10 w-10"
+                onClick={togglePlay}
+                disabled={!currentSong}
+              >
+                {isPlaying ? (
+                  <Pause className="h-5 w-5" />
+                ) : (
+                  <Play className="h-5 w-5" />
+                )}
+              </Button>
+
+              <Button
+                size="icon"
+                variant="ghost"
+                className="hover:text-white text-zinc-400 flex"
+                onClick={playNext}
+                disabled={!canGoNext}
+              >
+                <SkipForward className="h-4 w-4" />
+              </Button>
+            </div>
+            <Button
+              size="icon"
+              variant="ghost"
+              className="inline-flex hover:text-white text-zinc-400"
+            >
+              <Repeat className="h-4 w-4" />
+            </Button>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  size="icon"
+                  variant="ghost"
+                  className="hover:text-white text-zinc-400"
+                >
+                  <Volume1 className="h-7 w-7" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-3 bg-zinc-800 border-zinc-700">
+                <Slider
+                  className="h-32 hover:cursor-grab active:cursor-grabbing text-green-500"
+                  orientation="vertical"
+                  step={1}
+                  max={100}
+                  value={[volume]}
+                  onValueChange={(value) => setVolume(value[0])}
+                  trackClassName="w-1 bg-zinc-600"
+                  rangeClassName=" bg-green-500"
+                  thumbClassName="w-3 h-3 bg-white bg-green-500 border-none"
+                />
+                <div className="text-xs text-white text-center mt-2">
+                  {volume}%
+                </div>
+              </PopoverContent>
+            </Popover>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+
+      <MobileSheetFooter />
+    </>
   );
 });
---
+++ client-side/src/widgets/Layout/UI/AudioFooter/MobileSheetFooter.tsx
@@ -0,0 +1,199 @@
+import { Button } from "@/components/kit/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/kit/sheet";
+import { Slider } from "@/components/kit/slider";
+import { playerStore } from "@/entities/store/player-store";
+import { formatDuration } from "@/shared/lib/format/formatDuration";
+import {
+  Pause,
+  Play,
+  Repeat,
+  Shuffle,
+  SkipBack,
+  SkipForward,
+} from "lucide-react";
+import { observer } from "mobx-react-lite";
+import { useState } from "react";
+
+export const MobileSheetFooter = observer(() => {
+  const {
+    currentSong,
+    isPlaying,
+    togglePlay,
+    playNext,
+    playPrev,
+    currentTime,
+    duration,
+    seek,
+    canGoNext,
+    canGoPrev,
+  } = playerStore;
+  const [isHoverTime, setIsHoverTime] = useState(false);
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <footer className="sm:hidden h-20 bg-zinc-900 px-4 relative mx-4 rounded-lg">
+          <div className="absolute -top-13 left-0 right-0 flex justify-between text-md text-zinc-400 px-2">
+            {isHoverTime && (
+              <>
+                <span className="text-white border border-zinc-600 rounded-md px-3 py-2 bg-zinc-900">
+                  {formatDuration(currentTime)}
+                </span>
+                <span className="text-white border border-zinc-600 rounded-md px-3 py-2 bg-zinc-900">
+                  {formatDuration(duration)}
+                </span>
+              </>
+            )}
+          </div>
+
+          <div
+            className="absolute left-0 right-0 h-1"
+            onMouseEnter={() => setIsHoverTime(true)}
+            onMouseLeave={() => setIsHoverTime(false)}
+          >
+            <Slider
+              value={[currentTime]}
+              max={duration || 100}
+              step={1}
+              onValueChange={(value) => seek(value[0])}
+              className="w-full h-full hover:cursor-grab active:cursor-grabbing"
+              trackClassName="h-1 bg-zinc-600"
+              rangeClassName="bg-green-500"
+              thumbClassName="w-3 h-3 bg-green-500 border-none"
+            />
+          </div>
+
+          <div className="flex justify-between items-center h-full mx-auto">
+            <div className="flex items-center gap-4">
+              {currentSong && (
+                <>
+                  <img
+                    src={currentSong.imageUrl}
+                    alt={currentSong.title}
+                    className="w-14 h-14 object-cover rounded-md"
+                  />
+                  <div className="flex-1 min-w-0">
+                    <div className="font-medium truncate hover:underline cursor-pointer">
+                      {currentSong.title}
+                    </div>
+                    <div className="text-sm text-zinc-400 truncate hover:underline cursor-pointer">
+                      {currentSong.artist}
+                    </div>
+                  </div>
+                </>
+              )}
+            </div>
+
+            <div className="flex items-center gap-4">
+              <Button
+                size="icon"
+                className="bg-green-500 hover:bg-green-600 text-black rounded-full h-10 w-10"
+                onClick={togglePlay}
+                disabled={!currentSong}
+              >
+                {isPlaying ? (
+                  <Pause className="h-5 w-5" />
+                ) : (
+                  <Play className="h-5 w-5" />
+                )}
+              </Button>
+            </div>
+          </div>
+        </footer>
+      </SheetTrigger>
+      <SheetContent
+        side="bottom"
+        className="h-full bg-zinc-900 border-zinc-800 rounded-t-2xl"
+      >
+        <div className="flex flex-col h-full items-center pt-15 mx-7">
+          <div className="flex flex-col">
+            {currentSong && (
+              <>
+                <img
+                  src={currentSong.imageUrl}
+                  alt={currentSong.title}
+                  className="w-full object-cover rounded-xl shadow-2xl mb-4"
+                />
+                <div className="mb-8">
+                  <h2 className="font-medium text-lg truncate hover:underline cursor-pointer">
+                    {currentSong?.title}
+                  </h2>
+                  <p className="text-base text-zinc-400 truncate hover:underline cursor-pointer">
+                    {currentSong?.artist}
+                  </p>
+                </div>
+              </>
+            )}
+
+            <div className="mb-6">
+              <Slider
+                value={[currentTime]}
+                max={duration || 100}
+                step={1}
+                onValueChange={(value) => seek(value[0])}
+                className="w-full h-2"
+                trackClassName="h-1 bg-zinc-600"
+                rangeClassName="bg-green-500"
+                thumbClassName="w-4 h-4 bg-green-500 border-none"
+              />
+              <div className="flex justify-between text-xs text-zinc-400 mt-2 px-1">
+                <span>{formatDuration(currentTime)}</span>
+                <span>{formatDuration(duration)}</span>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-center gap-6 mb-8">
+              <Button
+                size="icon"
+                variant="ghost"
+                className="hover:text-white text-zinc-400"
+              >
+                <Shuffle className="h-6 w-6" />
+              </Button>
+
+              <Button
+                size="icon"
+                variant="ghost"
+                className="hover:text-white text-zinc-400"
+                onClick={playPrev}
+                disabled={!canGoPrev}
+              >
+                <SkipBack className="h-6 w-6" />
+              </Button>
+
+              <Button
+                size="icon"
+                className="bg-green-500 hover:bg-green-600 text-black rounded-full h-14 w-14"
+                onClick={togglePlay}
+                disabled={!currentSong}
+              >
+                {isPlaying ? (
+                  <Pause className="h-7 w-7" />
+                ) : (
+                  <Play className="h-7 w-7" />
+                )}
+              </Button>
+
+              <Button
+                size="icon"
+                variant="ghost"
+                className="hover:text-white text-zinc-400"
+                onClick={playNext}
+                disabled={!canGoNext}
+              >
+                <SkipForward className="h-6 w-6" />
+              </Button>
+
+              <Button
+                size="icon"
+                variant="ghost"
+                className="hover:text-white text-zinc-400"
+              >
+                <Repeat className="h-6 w-6" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+});
