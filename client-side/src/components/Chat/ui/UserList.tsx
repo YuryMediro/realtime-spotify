c02281ce4d496bb71756cb37c1d823228ee74050
Yury Mediro <sagn375@gmail.com>
2025-10-23 16:25:14 +0300

    feat: add last essage and time message UserList

--- client-side/src/components/Chat/ui/UserList.tsx
+++ client-side/src/components/Chat/ui/UserList.tsx
@@ -1,13 +1,20 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/kit/avatar";
 import { ScrollArea } from "@/components/kit/scroll-area";
 import { chatStore } from "@/entities/store/chat-store";
+import { formatTimeChat } from "@/shared/lib/format/formatTimeChat";
 import { UserListSkeleton } from "@/shared/ui/skeleton/UserListSkeleton";
 import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
 
 export const UserList = observer(() => {
-  const { users, isUserOnline, selectedUser, isLoading, setSelectedUser } =
-    chatStore;
+  const {
+    users,
+    isUserOnline,
+    selectedUser,
+    isLoading,
+    setSelectedUser,
+    getLastMessageForUser,
+  } = chatStore;
 
   return (
     <div className="border-r border-zinc-800 md:block hidden">
@@ -19,6 +26,7 @@ export const UserList = observer(() => {
             ) : (
               users.map((user) => {
                 const isOnline = isUserOnline(user.clerkId);
+                const lastMessage = getLastMessageForUser(user.clerkId);
                 return (
                   <>
                     <Link
@@ -28,7 +36,7 @@ export const UserList = observer(() => {
                     >
                       <div
                         onClick={() => setSelectedUser(user)}
-                        className={`flex items-center max-w-[280px] justify-center lg:justify-start gap-3 p-3
+                        className={`flex border items-center max-w-[210px] justify-center lg:justify-start gap-3 p-2.5
 										rounded-lg cursor-pointer transition-colors ${selectedUser?.clerkId === user.clerkId ? "bg-zinc-600" : "hover:bg-zinc-700"}`}
                       >
                         <div className="relative">
@@ -47,11 +55,21 @@ export const UserList = observer(() => {
                           />
                         </div>
 
-                        <div className="flex-1 min-w-0 lg:block hidden  truncate">
-                          <span className="font-medium truncate">
-                            {/* {user.fullName} */}
-                            Alienaaasdasdasdasdasdasdasdasasdasd
-                          </span>
+                        <div className="flex-1 min-w-0 lg:flex flex-col hidden truncate">
+                          <div className="flex justify-between items-center gap-1 mb-1">
+                            <span className="font-medium truncate text-sm flex-1">
+                              {user.fullName}
+                            </span>
+
+                            {lastMessage && (
+                              <span className="text-xs text-zinc-400 whitespace-nowrap flex-shrink-0">
+                                {formatTimeChat(lastMessage.createdAt)}
+                              </span>
+                            )}
+                          </div>
+                          <div className="text-xs text-zinc-400 truncate">
+                            {lastMessage ? lastMessage.content : "No messages"}
+                          </div>
                         </div>
                       </div>
                     </Link>
