2a055e7202b3025b5d4ca58af6596bed489e572a
Yury Mediro <sagn375@gmail.com>
2025-11-06 17:32:51 +0300

    refactor: delete bottom button

--- client-side/src/components/Chat/ui/Messages.tsx
+++ client-side/src/components/Chat/ui/Messages.tsx
@@ -68,7 +68,7 @@ export const Messages = observer(() => {
       <Button
         type="button"
         size="icon"
-        className="absolute lg:bottom-4 bottom-15 right-4 size-10 rounded-full p-2 shadow-lg bg-zinc-700/90 hover:bg-zinc-600/90 z-10"
+        className="absolute lg:bottom-4 right-4 size-10 rounded-full p-2 shadow-lg bg-zinc-700/90 hover:bg-zinc-600/90 z-10"
         onClick={scrollToBottom}
       >
         <ArrowDown className="text-white" />
