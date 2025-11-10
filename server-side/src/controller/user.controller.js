e374cb99f58ddc0a57d41880fba134b21f79e8ea
Yury Mediro <sagn375@gmail.com>
2025-11-10 16:56:18 +0300

    feat: usersWithLastMessages

--- server-side/src/controller/user.controller.js
+++ server-side/src/controller/user.controller.js
@@ -1,30 +1,49 @@
-import { User } from "../models/user.model.js";
-import { Message } from "../models/message.model.js";
+import { User } from '../models/user.model.js'
+import { Message } from '../models/message.model.js'
 
 export const getAllUsers = async (req, res, next) => {
 	try {
-		const currentUserId = req.auth.userId;
-		const users = await User.find({ clerkId: { $ne: currentUserId } });
-		res.status(200).json(users);
+		const currentUserId = req.auth.userId
+
+		const users = await User.find({ clerkId: { $ne: currentUserId } })
+
+		const usersWithLastMessages = await Promise.all(
+			users.map(async user => {
+				const lastMessage = await Message.findOne({
+					$or: [
+						{ senderId: currentUserId, receiverId: user.clerkId },
+						{ senderId: user.clerkId, receiverId: currentUserId },
+					],
+				}).sort({ createdAt: -1 })
+
+				const userObject = user.toObject()
+				return {
+					...userObject,
+					lastMessage: lastMessage ? lastMessage.toObject() : null,
+				}
+			})
+		)
+
+		res.status(200).json(usersWithLastMessages)
 	} catch (error) {
-		next(error);
+		next(error)
 	}
-};
+}
 
 export const getMessages = async (req, res, next) => {
 	try {
-		const myId = req.auth.userId;
-		const { userId } = req.params;
+		const myId = req.auth.userId
+		const { userId } = req.params
 
 		const messages = await Message.find({
 			$or: [
 				{ senderId: userId, receiverId: myId },
 				{ senderId: myId, receiverId: userId },
 			],
-		}).sort({ createdAt: 1 });
+		}).sort({ createdAt: 1 })
 
-		res.status(200).json(messages);
+		res.status(200).json(messages)
 	} catch (error) {
-		next(error);
+		next(error)
 	}
-};
+}
